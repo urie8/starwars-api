b45d8df918f0a29d5d04c7b653950e5b49bdd337
urie8 <dionpeci08@gmail.com>
2024-01-25 12:57:56 +0100

    some changes

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
 fetch("https://localhost:7279/api/Character").then((res) =>
-  res.json().then((data) => displayCharacters(data.characters))
+  res.json().then((data) => console.log(data))
 );
+
+getCharacters();
+
+function getCharacters() {
+  fetch("https://localhost:7279/api/Character").then((res) =>
+    res.json().then((data) => displayCharacters(data))
+  );
+}
 
 let charactersContainer = document.querySelector("#characters-container");
 
 function displayCharacters(characters) {
   characters.forEach((c) => {
-    charctersCon.innerHTML += `
-    <div class="user" id="${u.firstName}">
-        <h1>${u.firstName} ${u.lastName}</h1>
-        <h3>Age: ${u.age}</h3>
-        <h3>Username: ${u.username}</h3>
-        <h3>Address: ${u.address.address}</h3>
-        <img src="${u.image}" style="width:150px">
+    if (c.isGood === true) {
+      charactersContainer.innerHTML += `
+    <div class="character" id="${c.name}">
+        <h1>${c.name}</h1>
+        <h3>Alignment: Good</h3>
+        <h3>Species: ${c.species}</h3>
+        <h3>Height: ${c.height}m</h3>
+        <h3>Role: ${c.role}</h3>
     </div>`;
+    } else {
+      charactersContainer.innerHTML += `
+    <div class="character" id="${c.name}">
+        <h1>${c.name}</h1>
+        <h3>Alignment: Evil</h3>
+        <h3>Species: ${c.species}</h3>
+        <h3>Height: ${c.height}</h3>
+        <h3>Role: ${c.role}</h3>
+    </div>`;
+    }
   });
 }
